6ee6153d30b20db86dbf216712b8e8da1b01ad2f
sajith <sajith@salesfokuz.in>
2022-08-29 17:50:57 +0530

    timesheep get api updated

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 
 from fastapi import FastAPI,Form,File, UploadFile
+from io import BytesIO
+import pandas as pd 
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 from pydantic import BaseModel
@@ -19,8 +21,37 @@ async def get():
 
 @app.post('/timesheetupload')
 async def timesheetupload(empid:str =Form(), file : UploadFile = File(...)):
-    pass
+    try : 
+        contents = file.file.read()
+        buffer=BytesIO(contents)
+        df = pd.read_csv(buffer)
+        df['empid'] = empid
+    except Exception as e :
+        print ("Error is "+ str(e))
+        return False
+    finally : 
+        file.file.close()
 
+    try:
+        client.bhrama.timesheet.insert_many(df.to_dict('records'))
+    except Exception as e:
+        print(str(e))
+        return False
+    return True
+
+@app.get('/timesheet')
+async def timesheet():
+    filter = {}
+    project = {
+        '_id':0
+    }
+    try:
+        timesheet =list(client.bhrama.timesheet.find(filter,project))
+    except Exception as e:
+        print(str(e))
+    df=pd.DataFrame(timesheet)
+    df=df.groupby(['empid','Date']).sum().reset_index()
+    return df.to_dict('records')
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload = True ,debug = True)
