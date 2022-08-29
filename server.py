0a3ff735d34b6eaad5eb40919a095b769e85399e
sajith <sajith@salesfokuz.in>
2022-08-29 16:29:21 +0530

    initial comit

---
+++ server.py
@@ -0,0 +1,27 @@
+
+from fastapi import FastAPI,Form,File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+from pydantic import BaseModel
+import uvicorn
+client = MongoClient('mongodb://localhost:27017/')
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+    )
+@app.get('/')
+async def get():
+    return {'200' : "Success"}
+
+@app.post('/timesheetupload')
+async def timesheetupload(empid:str =Form(), file : UploadFile = File(...)):
+    
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload = True ,debug = True)
